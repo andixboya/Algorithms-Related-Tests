2fe5bda81fdfbe89d0d100091ff077b877589ede
andixboya <andreyanboyadzhiev@gmail.com>
2019-10-26 22:56:55 +0300

    Adding Recursion-related tasks

---
+++ 01.Recursion_Related/L01_Recursive_ArraySum/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+
+namespace L01_Recursive_ArraySum
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = arr[i];
+            }
+
+
+            GenerateAllNCombinations(arr, vector, n, arr.Length);
+
+
+
+        }
+
+        private static void GenerateAllNCombinations(int[] set, int[] vector, int index, int border)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+                return;
+            }
+            else
+            {
+                for (int i = border; i < set.Length; i++)
+                {
+                    vector[index] = set[i];
+                    GenerateAllNCombinations(set, vector, index+1, i+1);
+                }
+            }
+
+        }
+
+    
+    private static void GenerateVector(int[] arr, int index)
+    {
+        if (index > arr.Length - 1)
+        {
+            Console.WriteLine(string.Join("", arr));
+            return;
+        }
+
+        for (int i = 0; i <= 1; i++)
+        {
+            arr[index] = i;
+            GenerateVector(arr, index + 1);
+        }
+
+    }
+
+    private static void DrawFigure(int n)
+    {
+        if (n <= 0)
+        {
+            return;
+        }
+
+        Console.WriteLine(new string('*', n));
+        DrawFigure(n - 1);
+
+        Console.WriteLine(new string('#', n));
+
+    }
+
+    //2nd Recursive Factorial 
+    private static int GetFactioral(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        return (n * GetFactioral(n - 1));
+
+    }
+
+    //1st Sum Of Array
+    private static int GetSumOfArray(int[] array)
+    {
+        if (array.Length == 0)
+        {
+            return 0;
+        }
+
+        return array[0] + GetSumOfArray(array.Skip(1).ToArray());
+    }
+}
+}
---
+++ 01.Recursion_Related/L02_PathFinding/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L02_PathFinding
+{
+    class Program
+    {
+        private static int rowMax;
+        private static int colMax;
+
+        private static char[][] matrix;
+        private static List<char> roads = new List<char>();
+        static void Main(string[] args)
+        {
+            ReadMatrix();
+
+
+            FindPaths(0, 0, 'S');
+
+
+
+        }
+
+        private static void FindPaths(int row, int col, char direction)
+        {
+
+            //Console.WriteLine();
+            //PrintMatrix(matrix);
+            if (!IsInBounds(row, col))
+            {
+                return;
+            }
+
+            roads.Add(direction);
+
+            if (IsExit(row, col))
+            {
+                PrintPath();
+            }
+
+            else if (!IsVisited(row, col) && IsFree(row, col))
+            {
+                Mark(row, col);
+                FindPaths(row, col + 1, 'R');
+                FindPaths(row, col - 1, 'L');
+                FindPaths(row - 1, col, 'U');
+                FindPaths(row + 1, col, 'D');
+
+                //a bit strange.... 
+                Unmark(row, col);
+            }
+
+
+            //why is this... accomplished? 
+            //to remove it from the map collection
+            //in thee unmark it is removed from the matrix itself
+            //below we remove it from the map! 
+            roads.RemoveAt(roads.Count - 1);
+
+        }
+
+        private static void Mark(int row, int col)
+        {
+            matrix[row][col] = 'v';
+        }
+
+        private static void Unmark(int row, int col)
+        {
+            matrix[row][col] = '-';
+        }
+
+        private static bool IsFree(int row, int col)
+        {
+            return matrix[row][col] == '-';
+        }
+
+        private static bool IsVisited(int row, int col)
+        {
+            //mistake: here i thought they would be marked on the map (matrix) 
+            //return matrix[row][col] == 'R' ||
+            //       matrix[row][col] == 'L' ||
+            //       matrix[row][col] == 'U' ||
+            //       matrix[row][col] == 'D';
+
+            return matrix[row][col] == 'v';
+
+
+        }
+
+        private static bool IsExit(int row, int col)
+        {
+            return matrix[row][col] == 'e';
+        }
+
+        private static void PrintPath()
+        {
+            Console.WriteLine(string.Join("", roads.Skip(1)));
+        }
+
+        private static bool IsInBounds(int row, int col)
+        {
+
+            return row >= 0 && row <= rowMax - 1 &&
+                   col >= 0 && col <= colMax - 1
+                   && !IsVisited(row, col);
+        }
+
+        private static void ReadMatrix()
+        {
+            rowMax = int.Parse(Console.ReadLine());
+            colMax = int.Parse(Console.ReadLine());
+
+            matrix = new char[rowMax][];
+
+            for (int i = 0; i < rowMax; i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();
+            }
+
+
+        }
+
+        private static void PrintMatrix(char[][] matrixToPrint)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < matrixToPrint.Length; i++)
+            {
+                sb.AppendLine(string.Join("", matrixToPrint[i]));
+            }
+
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}

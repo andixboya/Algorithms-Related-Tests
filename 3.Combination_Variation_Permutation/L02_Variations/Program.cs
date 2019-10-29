cb549651b536b1d915e4188c080f2e5275a4a895
andixboya <andreyanboyadzhiev@gmail.com>
2019-10-29 15:02:50 +0200

    Uploading combination and variation related tests

---
+++ 3.Combination_Variation_Permutation/L02_Variations/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace L02_Variations
+{
+    class Program
+    {
+        public static char[] elements;
+        public static char[] variation;
+        public static bool[] used;
+
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                .Split(' ')
+                .Select(char.Parse)
+                .ToArray();
+            int maxCount = int.Parse(Console.ReadLine());
+
+            used = new bool[elements.Length];
+            variation = new char[maxCount];
+
+            GetVariations(0);
+
+            Console.WriteLine(GetVariationsCount(elements.Length,maxCount));
+        }
+
+        static void GetVariations(int index)
+        {
+            if (index >= variation.Length)
+            {
+                Console.WriteLine(string.Join(" ", variation));
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    //if this is documented , it adds variations with repetition 
+                    if (used[i] is false)
+                    {
+                        used[i] = true;
+
+                        variation[index] = elements[i];
+                        GetVariations(index + 1);
+
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        static long GetVariationsCount(int length, int boundary)
+        {
+            int belowPart = length - boundary;
+
+            return GetFactoriel(length) / GetFactoriel(belowPart);
+        }
+
+        static int GetFactoriel(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number* GetFactoriel(number-1);
+        }
+    }
+}
---
+++ 3.Combination_Variation_Permutation/L03Combinations/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace L03Combinations
+{
+    class Program
+    {
+        private static int bound;
+        private static int length;
+        private static char[] temp;
+        private static char[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+               .Split(' ')
+               .Select(char.Parse)
+               .ToArray();
+
+
+            bound = int.Parse(Console.ReadLine());
+            temp = new char[bound];
+            length = elements.Length;
+            GetCombination(0, 0);
+            Console.WriteLine(GetCombinationsCount(length,bound));
+
+        }
+
+        static void GetCombination(int index, int start)
+        {
+            if (index >= bound)
+            {
+                Console.WriteLine(string.Join(" ", temp));
+            }
+            
+            else
+            {
+                for (int i = start; i < length; i++)
+                {
+                    temp[index] = elements[i];
+
+                    //without repetition
+                    GetCombination(index + 1, i + 1);
+
+                    //with repetition
+                    //GetCombination(index + 1, i);
+                }
+            }
+        }
+
+
+        static int GetCombinationsCount(int length, int bound)
+        {
+            int belowNumber = GetFactoriel(bound) / GetFactoriel(length - bound);
+            
+            return GetFactoriel(length) / belowNumber;
+        }
+
+        static int GetFactoriel(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * GetFactoriel(number - 1);
+        }
+    }
+}

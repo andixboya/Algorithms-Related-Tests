d27ad1380e8c7db6cacdf79ccd7fad17d9e5561b
andixboya <andreyanboyadzhiev@gmail.com>
2019-11-03 19:42:21 +0200

    Adding 2 tasks related to DP

---
+++ 5.Dynamic Optimization/L01_DP_Fibonnaci/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace L01_DP_Fibonnaci
+{
+    public class Program
+    {
+
+        public static long[] solutions;
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            solutions = new long[number+1];
+
+            long answer = GetFibbonaci(number);
+
+            Console.WriteLine(answer);
+        }
+
+        public static long GetFibbonaci(int number)
+        {
+            if (solutions[number] != 0)
+            {
+                return solutions[number];
+            }
+
+            if (number == 1 || number == 2)
+            {    
+                return 1;
+            }
+
+            //here just separate the result into a variable and save it in solutions array, for the future!
+            long result = GetFibbonaci(number - 1) + GetFibbonaci(number - 2);
+
+            solutions[number] = result;
+
+            return result ;
+            
+            //alternative is just returning the results always
+            //return GetFibbonaci(number-1)+ GetFibbonaci(number-2);
+
+        }
+    }
+}
---
+++ 5.Dynamic Optimization/L02_LIS/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L02_LIS
+{
+    class Program
+    {
+        private static int[] seq;
+        private static int[] len;
+        //we`ll need this , to keep a reference between the answer-indicies
+        private static int[] prev;
+
+
+        static void Main()
+        {
+
+            seq = Console.ReadLine()
+                .Split(' ')
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(int.Parse)
+                .ToArray();
+
+            len = new int[seq.Length];
+
+            prev = new int[seq.Length];
+
+
+            var result = GetMaxSubsequence();
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int[] GetMaxSubsequence()
+        {
+            int maxLen = 0;
+            int lastIndex = -1;
+            for (int rightMost = 0; rightMost < seq.Length; rightMost++)
+            {
+
+                //for each element to the right, the length is set to 1 and
+                //the previous is set to non existent
+                len[rightMost] = 1;
+                prev[rightMost] = -1;
+
+                //right-most 
+                var currentEl = seq[rightMost];
+
+                //note: **** in each count prev holds our answers to the longest and it is rewritten!
+                for (int leftMost = 0; leftMost < rightMost; leftMost++)
+                {
+                    var previousEl = seq[leftMost];
+
+                    //if there is an element, which is smaller than the current rightMost
+                    //we`ll add +1 to the result
+                    //why +1?=> because otherwise it would be from another sequence
+                    if ((previousEl < currentEl) && (len[leftMost] + 1 > len[rightMost]))
+                    {
+                        //you overwrite only the value of the right pointer!
+                        len[rightMost] = len[leftMost] + 1;
+                        prev[rightMost] = leftMost;
+                    }
+                }
+
+
+                //and here we rewrite if the rightMostLeng is bigger than the previous rightMost
+                //for each iteration  (we check if the last index is not longest!
+
+                if (len[rightMost] > maxLen)
+                {
+                    maxLen = len[rightMost];
+                    lastIndex = rightMost;
+                }
+            }
+
+            return RestoreLIS(lastIndex);
+        }
+
+        public static int[] RestoreLIS(int lastIndex)
+        {
+            var longestSeq = new List<int>();
+            while (lastIndex != -1)
+            {
+                //from seq we take the value which we need
+                longestSeq.Add(seq[lastIndex]);
+                //from prev we keep a reference to each previous element to the  last
+                lastIndex = prev[lastIndex];
+            }
+
+            longestSeq.Reverse();
+            return longestSeq.ToArray();
+        }
+
+
+       
+    }
+}
